8056fb88bc4447648e3575a76874a901b35fe780
Benpsheppard <ben@bensheppard.co.uk>
2025-09-20 12:35:28 +0100

    Middleware added

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 // Imports
 const dotenv = require('dotenv').config(); // allow reading of .env file
 const express = require('express');  // import express
-const router = require('./routes/workoutRoutes.js');
+const { router } = require('./routes/workoutRoutes.js'); // import workout routes
+const { errorHandler } = require('./middleware/errorMiddleware.js');    // import error handler middleware
 
 // Variables
 const port = process.env.PORT || 5050;  // port from .env file or default to 5050
@@ -12,8 +13,15 @@ const port = process.env.PORT || 5050;  // port from .env file or default to 505
 // Initialise app
 const app = express();
 
+// Middleware
+app.use(express.json());    // middleware to allow server to read json data
+app.use(express.urlencoded({ extended: false }));      // middleware to allow server to read urlencoded data
+
 // GET route
 app.use('/api/workouts', router);
+
+// Custom Error Handler initialisation
+app.use(errorHandler);
 
 // Port listener
 app.listen(port, () => {
