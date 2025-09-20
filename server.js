7978496c9c17d7311b4123dc7b137892f8b26726
Benpsheppard <ben@bensheppard.co.uk>
2025-09-20 17:41:07 +0100

    Added database connection

--- server.js
+++ server.js
@@ -3,12 +3,17 @@
 
 // Imports
 const dotenv = require('dotenv').config(); // allow reading of .env file
+const colors = require('colors');   // import colors for CLI aesthetics
+const connectDB = require('./config/db.js');    // import database configuration file
 const express = require('express');  // import express
 const { router } = require('./routes/workoutRoutes.js'); // import workout routes
 const { errorHandler } = require('./middleware/errorMiddleware.js');    // import error handler middleware
 
 // Variables
 const port = process.env.PORT || 5050;  // port from .env file or default to 5050
+
+// Connect to database
+connectDB();
 
 // Initialise app
 const app = express();
