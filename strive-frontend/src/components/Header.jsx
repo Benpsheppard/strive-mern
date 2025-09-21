844a74c0847016497a5c2323edde6a856b567eb1
Benpsheppard <ben@bensheppard.co.uk>
2025-09-21 21:18:05 +0100

    Header for pages created

---
+++ strive-frontend/src/components/Header.jsx
@@ -0,0 +1,56 @@
+// Header.jsx
+
+// Imports
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const navOps = "text=[#EDF2F4] hover:text-[#EF233C] transition-colors";
+
+    return (
+        <header className="header fixed left-0 w-full bg-[#2B2D42] backdrop-blur-md text-[#EDF2F4] shadow-[0_8px_30px_rgba(0,0,0,0.5)] z-100">
+            <div className="container mx-auto flex items-center justify-between px-6 py-4">
+                
+                {/* Logo / Title */}
+                <div className="text-2xl font-bold tracking-wide">
+                    <Link to="/" className={navOps}>
+                        STRIVE
+                    </Link>
+                </div>
+
+                {/* Navbar */}
+                <nav>
+                <ul className="flex space-x-6 text-lg">
+                    <li>
+                        <Link to="/new-workout" className={navOps}>
+                            New Workout
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/prev-workouts" className={navOps}>
+                            Previous Workouts
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/progress" className={navOps}>
+                            Progress
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className={navOps}>
+                            Contact
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/login' className={navOps}>
+                            Sign in
+                        </Link>
+                    </li>
+                </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+// Export Header
+export default Header;
