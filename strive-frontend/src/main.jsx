9987045d644583fcacb300a880b96e1248188337
Benpsheppard <ben@bensheppard.co.uk>
2025-09-21 21:17:23 +0100

    Comments improved

--- strive-frontend/src/main.jsx
+++ strive-frontend/src/main.jsx
@@ -1,8 +1,13 @@
+// main.jsx
+// File to control main functionality
+
+// Imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Root
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
