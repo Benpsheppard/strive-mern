5da9be02cafb7fef70b55b8fe223f90418e62ed7
Benpsheppard <ben@bensheppard.co.uk>
2025-09-22 20:59:30 +0100

    Added store and provider

--- strive-frontend/src/main.jsx
+++ strive-frontend/src/main.jsx
@@ -4,12 +4,16 @@
 // Imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
 import './index.css'
 import App from './App.jsx'
 
 // Root
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
