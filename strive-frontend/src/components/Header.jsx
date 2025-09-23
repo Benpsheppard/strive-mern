a26b74acd0f91512b581b7af80f4db1db25963cd
Benpsheppard <ben@bensheppard.co.uk>
2025-09-23 16:48:21 +0100

    Updated header to display user's username when logged in

--- strive-frontend/src/components/Header.jsx
+++ strive-frontend/src/components/Header.jsx
@@ -4,7 +4,12 @@
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const navOps = "text=[#EDF2F4] hover:text-[#EF233C] transition-colors";
+    // Navigation options styling
+    const navOps = "text-[#EDF2F4] hover:text-[#EF233C] inline-block transform transition duration-200 hover:scale-110";
+
+    // Get user from localStorage
+    const storedUser = localStorage.getItem("user");
+    const user = storedUser ? JSON.parse(storedUser) : null;
 
     return (
         <header className="header fixed left-0 w-full bg-[#2B2D42] backdrop-blur-md text-[#EDF2F4] shadow-[0_8px_30px_rgba(0,0,0,0.5)] z-100">
@@ -19,33 +24,38 @@ const Header = () => {
 
                 {/* Navbar */}
                 <nav>
-                <ul className="flex space-x-6 text-lg">
-                    <li>
-                        <Link to="/new-workout" className={navOps}>
-                            New Workout
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/prev-workouts" className={navOps}>
-                            Previous Workouts
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/progress" className={navOps}>
-                            Progress
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" className={navOps}>
-                            Contact
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/login' className={navOps}>
-                            Sign in
-                        </Link>
-                    </li>
-                </ul>
+                    <ul className="flex space-x-6 text-lg">
+                        <li>
+                            <Link to="/new-workout" className={navOps}>
+                                New Workout
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/prev-workouts" className={navOps}>
+                                Previous Workouts
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/progress" className={navOps}>
+                                Progress
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" className={navOps}>
+                                Contact
+                            </Link>
+                        </li>
+                        <li>
+                            {user ? (
+                                <Link to='/profile' className="text-[#EF233C] hover:text-[#D90429] transition-colors transition hover:scale-105">
+                                    {user.username}
+                                </Link>
+                            ) : (
+                                <Link to='/login' className={navOps}>
+                                    Sign in
+                                </Link>)}
+                        </li>
+                    </ul>
                 </nav>
             </div>
         </header>
