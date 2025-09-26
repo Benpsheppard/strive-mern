6adb8c2ba770f745c2f1b60cd314079b2c0a4b21
Benpsheppard <ben@bensheppard.co.uk>
2025-09-26 16:21:57 +0100

    Added workout reducer

--- strive-frontend/src/app/store.js
+++ strive-frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice.js';
+import workoutReducer from '../features/workouts/workoutsSlice.js';
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        workout: workoutReducer
     }
 });
