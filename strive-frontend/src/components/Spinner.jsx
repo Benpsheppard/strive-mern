799a4181e5f0cad2feacb597856ffde735f7c1d3
Benpsheppard <ben@bensheppard.co.uk>
2025-09-27 17:41:04 +0100

    Added loading icon

---
+++ strive-frontend/src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+// Spinner.jsx
+
+const Spinner = () => {
+    return (
+        <div className="loadingSpinnerContainer">
+            <div className="loadingSpinner"></div>
+        </div>
+    )
+};
+
+// Export
+export default Spinner;
