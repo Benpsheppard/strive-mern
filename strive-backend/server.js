f62762f8e7524570488c7a0f07fa6c50ec0b919f
Benpsheppard <ben@bensheppard.co.uk>
2025-10-08 16:45:18 +0100

    Fixed for deployment

--- strive-backend/server.js
+++ strive-backend/server.js
@@ -36,8 +36,8 @@ app.use(errorHandler);
 // Deployment configuration
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/strive-frontend/dist')));
-    app.get('/:catchAll(*)', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'strive-frontend', 'dist', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'strive-frontend', 'dist', 'index.html'));
     })
 }
 
